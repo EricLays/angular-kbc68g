cf7212b566bc21652e8144c9a75bef3ea2b312a2
EricLays <ericlays26@gmail.com>
2020-04-16 18:43:24 +0700

    2 files modified - Materi 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Eric';
+  item =
+  [
+    {
+      nama : 'Buah kelengkeng',
+      harga : 10000
+    },
+    {
+      nama : 'jeruk',
+      harga : 15000
+    }
+  ]
+  user = 
+  
+    {
+      nama : ''
+    };
+  
+  itemArr = [1,2,3,4];
+  show : boolean = true;
 }
